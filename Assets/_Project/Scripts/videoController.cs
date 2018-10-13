d146fc6129a72ea004db3005015e3056de2bdc9c
Jonathan Wortington <worthingtonjg@gmail.com>
2018-10-12 22:24:43 -0600

    build

--- Assets/_Project/Scripts/videoController.cs
+++ Assets/_Project/Scripts/videoController.cs
@@ -12,9 +12,25 @@ public class videoController : MonoBehaviour {
 	// Use this for initialization
 	public Flowchart fungus;
 
+	public string videoFile;
+
 	void Start () {
+		string url = string.Empty;
+
+		if (Application.platform == RuntimePlatform.WebGLPlayer)
+		{
+			url = System.IO.Path.Combine (Application.streamingAssetsPath, videoFile);
+		}
+		else
+		{
+			url = System.IO.Path.Combine (Application.streamingAssetsPath, videoFile);
+		}
+			
+
 		videoPlayer = GetComponent<VideoPlayer>();
 		videoPlayer.loopPointReached += EndReached;
+		videoPlayer.url = url;
+		videoPlayer.Play();
 	}
 
     private void EndReached(VideoPlayer source)
